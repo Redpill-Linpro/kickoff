cbbf2677e34098f78c2cfd532a20b774dec5ffd5
Espen Braastad <espen@redpill-linpro.com>
2013-09-26 12:49:12 +0200

    Improved gitsh logging

--- kickoff/app/gitsh/__init__.py
+++ kickoff/app/gitsh/__init__.py
@@ -31,7 +31,8 @@ class gitsh():
     def init(self):
         s = False
     
-        pr = subprocess.Popen(['git', 'init', self.cache],
+        cmd = ['git', 'init', self.cache]
+        pr = subprocess.Popen(cmd,
                cwd=os.path.dirname(self.cache),
                stdout=subprocess.PIPE, 
                stderr=subprocess.PIPE, 
@@ -45,8 +46,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to initialized repository at %s" % \
                 self.cache)
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
 
@@ -57,7 +58,8 @@ class gitsh():
 
         s = False
     
-        pr = subprocess.Popen(['git', 'remote', 'add', 'origin', remote],
+        cmd = ['git', 'remote', 'add', 'origin', remote]
+        pr = subprocess.Popen(cmd,
                cwd=self.cache,
                stdout=subprocess.PIPE, 
                stderr=subprocess.PIPE, 
@@ -71,8 +73,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to add remote origin repository " \
                 "%s" % remote)
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
 
@@ -80,7 +82,8 @@ class gitsh():
         s = False
     
         try:
-            pr = subprocess.Popen(['git', 'clone', self.repository, self.cache],
+            cmd = ['git', 'clone', self.repository, self.cache]
+            pr = subprocess.Popen(cmd,
                    cwd=os.path.dirname(self.cache),
                    stdout=subprocess.PIPE, 
                    stderr=subprocess.PIPE, 
@@ -99,8 +102,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to clone repository %s to %s" % \
                 (self.repository, self.cache))
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
     
@@ -108,6 +111,7 @@ class gitsh():
         s = False
     
         try:
+            cmd = ['git', 'pull']
             pr = subprocess.Popen(['git', 'pull'],
                    cwd=self.cache,
                    stdout=subprocess.PIPE, 
@@ -124,8 +128,8 @@ class gitsh():
             s = True
         else:
             self._dolog(logging.ERROR, "Failed to pull repository %s" % (self.cache))
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
 
         return (s,out,error,pr.returncode)
     
@@ -133,7 +137,8 @@ class gitsh():
         s = False
     
         try:
-            pr = subprocess.Popen(['git', 'add', path],
+            cmd = ['git', 'add', path]
+            pr = subproceYss.Popen(cmd,
                    cwd=self.cache,
                    stdout=subprocess.PIPE, 
                    stderr=subprocess.PIPE, 
@@ -151,8 +156,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to add %s to repository %s" % \
                 (path, self.cache))
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
        
@@ -160,7 +165,8 @@ class gitsh():
         s = False
 
         try:
-            pr = subprocess.Popen(['git', 'commit', '-m', message, path],
+            cmd = ['git', 'commit', '-m', message, path]
+            pr = subprocess.Popen(cmd,
                    cwd=self.cache,
                    stdout=subprocess.PIPE, 
                    stderr=subprocess.PIPE, 
@@ -178,8 +184,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to commit %s to repository %s" % \
                     (path, self.cache))
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, " \
+                "stderr=%s, retcode=%s" % (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
     
@@ -187,7 +193,8 @@ class gitsh():
         s = False
 
         try:
-            pr = subprocess.Popen(['git', 'push', '-u', 'origin', 'master'],
+            cmd = ['git', 'push', '-u', 'origin', 'master']
+            pr = subprocess.Popen(cmd,
                    cwd=self.cache,
                    stdout=subprocess.PIPE, 
                    stderr=subprocess.PIPE, 
@@ -205,8 +212,8 @@ class gitsh():
         else:
             self._dolog(logging.ERROR, "Failed to push repository %s" % \
                     (self.cache))
-            self._dolog(logging.DEBUG, "s=%s, out=%s, error=%s, ret=%s" % \
-                (s, out, error, pr.returncode))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
     
         return (s,out,error,pr.returncode)
 
@@ -215,16 +222,17 @@ class gitsh():
         log = []
     
         try:
-            pr = subprocess.Popen(['git', 'log', '--format= \
-                                                      author_name: %an\n \
-                                                      author_email: %ae\n \
-                                                      author_date: %at\n \
-                                                      committer_name: %cn\n \
-                                                      committer_email: %ce\n \
-                                                      committer_date: %ct\n \
-                                                      subject: %s\n \
-                                                      body: %b\n \
-                                                      \n'],
+            cmd = ['git', 'log', '--format= \
+                                    author_name: %an\n \
+                                    author_email: %ae\n \
+                                    author_date: %at\n \
+                                    committer_name: %cn\n \
+                                    committer_email: %ce\n \
+                                    committer_date: %ct\n \
+                                    subject: %s\n \
+                                    body: %b\n \
+                                    \n']
+            pr = subprocess.Popen(cmd,
                    cwd=self.cache,
                    stdout=subprocess.PIPE, 
                    stderr=subprocess.PIPE, 
@@ -248,6 +256,12 @@ class gitsh():
                         l[m.group(1).replace(' ','')] = m.group(2)
                 if len(l) > 6:
                     log.append(l)
+
+        else:
+            self._dolog(logging.ERROR, "Failed to read git log" % \
+                    (self.cache))
+            self._dolog(logging.DEBUG, "status=%s, cmd=%s, stdout=%s, stderr=%s, retcode=%s" % \
+                (s, " ".join(cmd), out, error, pr.returncode))
                         
         # Sort by commit date
         log = sorted(log, key=lambda k: k['committer_date']) 
